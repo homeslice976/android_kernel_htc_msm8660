fc9556c89a041cb4c8804b42f17ae645532ab93e
homeslice976 <homeslice976@gmail.com>
2013-02-08 12:50:40 -0500

    More OC and some clean up

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -72,7 +72,7 @@
 #define MAX_VDD_SC		1350000 /* uV */
 #define MIN_VDD_SC		 750000 /* uV */
 #define MAX_VDD_MEM		1350000 /* uV */
-#define MAX_VDD_DIG		1200000 /* uV */
+#define MAX_VDD_DIG		1300000 /* uV */
 #define MAX_AXI			 310500 /* KHz */
 #define SCPLL_LOW_VDD_FMAX	 594000 /* KHz */
 #define SCPLL_LOW_VDD		1000000 /* uV */
@@ -217,10 +217,10 @@ static struct clkctl_l2_speed l2_freq_tbl_v2[] = {
 	[16] = {1242000,  1, 0x17, 1200000, 1212500, 3},
 	[17] = {1296000,  1, 0x18, 1200000, 1225000, 3},
 	[18] = {1350000,  1, 0x19, 1200000, 1225000, 3},
-	[19] = {1404000,  1, 0x1A, 1200000, 1250000, 4},//4=L2 boost, unstable @ high clocks, therefore disabled over 1536mhz
-	[20] = {1458000,  1, 0x1B, 1225000, 1275000, 3},
-	[21] = {1512000,  1, 0x1C, 1225000, 1275000, 3},
-	[22] = {1566000,  1, 0x1D, 1225000, 1275000, 3},
+	[19] = {1404000,  1, 0x1A, 1200000, 1250000, 3},
+	[20] = {1458000,  1, 0x1B, 1225000, 1275000, 4},
+	[21] = {1512000,  1, 0x1C, 1225000, 1275000, 4},
+	[22] = {1566000,  1, 0x1D, 1225000, 1275000, 4},
 };
 
 #define L2(x) (&l2_freq_tbl_v2[(x)])
@@ -249,15 +249,12 @@ static struct clkctl_acpu_speed acpu_freq_tbl_oc[] = {
   { {1, 1}, 1296000,  ACPU_SCPLL, 0, 0, 1, 0x18, L2(17), 1125000, 0x03006000},
   { {1, 1}, 1350000,  ACPU_SCPLL, 0, 0, 1, 0x19, L2(18), 1150000, 0x03006000},
   { {1, 1}, 1404000,  ACPU_SCPLL, 0, 0, 1, 0x1A, L2(19), 1175000, 0x03006000},
-  { {1, 1}, 1458000,  ACPU_SCPLL, 0, 0, 1, 0x1B, L2(19), 1187500, 0x03006000},
-  { {1, 1}, 1536000,  ACPU_SCPLL, 0, 0, 1, 0x1C, L2(19), 1200000, 0x03006000},
-  { {1, 1}, 1566000,  ACPU_SCPLL, 0, 0, 1, 0x1D, L2(20), 1225000, 0x03006000},
-  { {1, 1}, 1620000,  ACPU_SCPLL, 0, 0, 1, 0x1E, L2(20), 1250000, 0x03006000},
-  { {1, 1}, 1674000,  ACPU_SCPLL, 0, 0, 1, 0x1F, L2(20), 1275000, 0x03006000},
-  { {1, 1}, 1728000,  ACPU_SCPLL, 0, 0, 1, 0x20, L2(21), 1300000, 0x03006000},
-  { {1, 1}, 1782000,  ACPU_SCPLL, 0, 0, 1, 0x21, L2(21), 1325000, 0x03006000},
-  { {1, 1}, 1836000,  ACPU_SCPLL, 0, 0, 1, 0x22, L2(21), 1337500, 0x03006000},
-  { {1, 1}, 1890000,  ACPU_SCPLL, 0, 0, 1, 0x23, L2(21), 1350000, 0x03006000},
+  { {1, 1}, 1458000,  ACPU_SCPLL, 0, 0, 1, 0x1B, L2(20), 1187500, 0x03006000},
+  { {1, 1}, 1512000,  ACPU_SCPLL, 0, 0, 1, 0x1C, L2(21), 1200000, 0x03006000},
+  { {1, 1}, 1566000,  ACPU_SCPLL, 0, 0, 1, 0x1D, L2(22), 1225000, 0x03006000},
+  { {1, 1}, 1620000,  ACPU_SCPLL, 0, 0, 1, 0x1E, L2(22), 1250000, 0x03006000},
+  { {1, 1}, 1674000,  ACPU_SCPLL, 0, 0, 1, 0x1F, L2(22), 1275000, 0x03006000},
+  { {1, 1}, 1728000,  ACPU_SCPLL, 0, 0, 1, 0x20, L2(22), 1300000, 0x03006000},
   { {0, 0}, 0 },
 };
 
@@ -873,57 +870,33 @@ static __init struct clkctl_acpu_speed *select_freq_plan(void)
 	pr_info("pte_efuse=0x%X, speed_bin=0x%X, pvs=0x%X\n", pte_efuse, speed_bin, pvs);
 
 	if (speed_bin == 0x2) {
-		max_khz = 1536000;
+		max_khz = 1728000;
 		switch (pvs) {
 		case 0x7:
 		case 0x5:
-			acpu_freq_tbl = acpu_freq_tbl_oc;
-			pr_info("ACPU PVS: Slowest\n");
-			break;
 		case 0x4:
-			acpu_freq_tbl = acpu_freq_tbl_oc;
-			pr_info("ACPU PVS: Slower\n");
-			break;
 		case 0x0:
-			acpu_freq_tbl = acpu_freq_tbl_oc;
-			pr_info("ACPU PVS: Slow\n");
-			break;
 		case 0x1:
-			acpu_freq_tbl = acpu_freq_tbl_oc;;
-			pr_info("ACPU PVS: Nominal\n");
-			break;
 		case 0x3:
-			acpu_freq_tbl = acpu_freq_tbl_oc;;
-			pr_info("ACPU PVS: Fast\n");
-			break;
 		default:
 			acpu_freq_tbl = acpu_freq_tbl_oc;
-			pr_warn("ACPU PVS: Unknown. Defaulting to slowest.\n");
+			pr_warn("ACPU PVS: OC\n");
 			break;
 		}
 	} else if (speed_bin == 0x1) {
-		max_khz = 1536000;
+		max_khz = 1728000;
 		switch (pvs) {
 		case 0x0:
 		case 0x7:
-			acpu_freq_tbl = acpu_freq_tbl_oc;
-			pr_info("ACPU PVS: Slow\n");
-			break;
 		case 0x1:
-			acpu_freq_tbl = acpu_freq_tbl_oc;
-			pr_info("ACPU PVS: Nominal\n");
-			break;
 		case 0x3:
-			acpu_freq_tbl = acpu_freq_tbl_oc;
-			pr_info("ACPU PVS: Fast\n");
-			break;
 		default:
 			acpu_freq_tbl = acpu_freq_tbl_oc;
-			pr_warn("ACPU PVS: Unknown. Defaulting to slow.\n");
+			pr_warn("ACPU PVS: OC\n");
 			break;
 		}
 	} else {
-		max_khz = 1536000;
+		max_khz = 1728000;
 		acpu_freq_tbl = acpu_freq_tbl_oc;
 	}
 
