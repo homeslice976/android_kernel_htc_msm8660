a590f087e816a9660964ea0754596c796bb91870
homeslice976 <homeslice976@gmail.com>
2013-02-14 12:04:55 -0500

    fix .59 patch

--- drivers/target/tcm_fc/tfc_sess.c
+++ drivers/target/tcm_fc/tfc_sess.c
@@ -474,6 +474,7 @@ static void ft_sess_rcu_free(struct rcu_head *rcu)
 {
 	struct ft_sess *sess = container_of(rcu, struct ft_sess, rcu);
 
+	transport_deregister_session(sess->se_sess);
 	kfree(sess);
 }
 
@@ -481,7 +482,6 @@ static void ft_sess_free(struct kref *kref)
 {
 	struct ft_sess *sess = container_of(kref, struct ft_sess, kref);
 
-	transport_deregister_session(sess->se_sess);
 	call_rcu(&sess->rcu, ft_sess_rcu_free);
 }
 
